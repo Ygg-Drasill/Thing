0845682c2ced815f21699eb44a0e5ae378aac0cc
MathiasFM <mathiasmogensen1@gmail.com>
2024-03-27 23:59:05 +0100

    penalty selector added

--- src/pages/HomePage.go
+++ src/pages/HomePage.go
@@ -27,12 +27,26 @@ func HomePage(context *gin.Context) {
 		fineStr = ""
 	}
 
+	penalty := map[string]int{
+		"§1 Gasbøde":                 2,
+		"§2 Dummebøde":               2,
+		"§3 Spildebøde":              2,
+		"§4 Push to main":            25,
+		"§5 Glemme bøde":             5,
+		"§6 Fremmøde med tømmermænd": 5,
+		"§7 Dårligt menneske bøde":   2,
+		"§8 Mangel af kilder":        2,
+		"§9 AI":                      20,
+		"§10 Bailer bøde":            20,
+	}
+
 	data := struct {
 		Title          string
 		Header         string
 		Items          []string
 		Person         []string
 		SelectedPerson string
+		Penalty        map[string]int
 		Fine           string
 		Fines          map[string]string
 	}{
@@ -41,6 +55,7 @@ func HomePage(context *gin.Context) {
 		Items:          []string{"You", "Will", "Pay"},
 		Person:         []string{"Androkles", "Alexander", "Mathias", "Gustav", "Tobias", "Mikael"},
 		SelectedPerson: selectedPersonStr,
+		Penalty:        penalty,
 		Fine:           fineStr,
 		Fines:          make(map[string]string),
 	}
