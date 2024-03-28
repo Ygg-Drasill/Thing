75682e8585fad39830ec8cb70bb8d4a03b0dc3cd
MathiasFM <mathiasmogensen1@gmail.com>
2024-03-28 01:14:40 +0100

    use penaltymap

--- src/api/handlers/PenaltiesHandler.go
+++ src/api/handlers/PenaltiesHandler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/Ygg-Drasill/Thing/src/penalties"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
@@ -12,18 +13,7 @@ import (
 func PenaltiesHandler(context *gin.Context) {
 	session := sessions.Default(context)
 
-	penalty := map[string]int{
-		"§1 Gasbøde":                 2,
-		"§2 Dummebøde":               2,
-		"§3 Spildebøde":              2,
-		"§4 Push to main":            25,
-		"§5 Glemme bøde":             5,
-		"§6 Fremmøde med tømmermænd": 5,
-		"§7 Dårligt menneske bøde":   2,
-		"§8 Mangel af kilder":        2,
-		"§9 AI":                      20,
-		"§10 Bailer bøde":            20,
-	}
+	penalty := penalties.PenaltyMap
 
 	data := struct {
 		Person    []string
--- src/pages/HomePage.go
+++ src/pages/HomePage.go
@@ -3,6 +3,7 @@ package pages
 import (
 	"net/http"
 
+	"github.com/Ygg-Drasill/Thing/src/penalties"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
@@ -18,18 +19,7 @@ func HomePage(context *gin.Context) {
 		selectedPersonStr = ""
 	}
 
-	penalty := map[string]int{
-		"§1 Gasbøde":                 2,
-		"§2 Dummebøde":               2,
-		"§3 Spildebøde":              2,
-		"§4 Push to main":            25,
-		"§5 Glemme bøde":             5,
-		"§6 Fremmøde med tømmermænd": 5,
-		"§7 Dårligt menneske bøde":   2,
-		"§8 Mangel af kilder":        2,
-		"§9 AI":                      20,
-		"§10 Bailer bøde":            20,
-	}
+	penalty := penalties.PenaltyMap
 
 	data := struct {
 		Title          string
